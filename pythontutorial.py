443c6fae78e5ab6eac4302686c5e468730587117
kentoy <kentoyfueconcillo@gmail.com>
2024-07-23 01:23:07 +0800

    var dec

--- pythontutorial.py
+++ pythontutorial.py
@@ -1,3 +1,10 @@
 # sample commit
 # sample 
 # sample commit 
+
+# String = "a b c d e f g / alpabeth"
+# integer = 1 2 3 4 5 / numeric
+
+x = "I am borskie and i am {} years old {}".format(1, 'hj')
+y = "I am borskie and i am " + "1" + "years old " + " " + "hj"
+print(y)
